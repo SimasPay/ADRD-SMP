8b4083470080aebbb2498c18edab3e482681578b
wdnk <widy.agung@gmail.com>
2017-05-08 21:56:37 +0700

    SMP-859 crash bug fixed

--- SimasPay/app/src/main/java/com/payment/simaspay/Cash_InOut/CashOutDetailsActivity.java
+++ SimasPay/app/src/main/java/com/payment/simaspay/Cash_InOut/CashOutDetailsActivity.java
@@ -136,7 +136,11 @@ public class CashOutDetailsActivity extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Boolean is50k = (Integer.parseInt(amount.getText().toString().replace("Rp ", "")) % 50000) == 0;
+                int txtamount=0;
+                if(!amount.getText().toString().equals("")){
+                    txtamount = Integer.parseInt(amount.getText().toString().replace("Rp ", ""));
+                }
+                Boolean is50k = (txtamount % 50000) == 0;
                 //Log.d("test","test kelipatan 50k: "+is50k);
                 if(sharedPreferences.getInt(Constants.PARAMETER_USERTYPE,-1)==Constants.CONSTANT_BANK_INT){
                     if (number.getText().toString().replace(" ", "").length() <= 0) {
