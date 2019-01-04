6f86f300f68967c9de146da15f521a8a90a94365
wdyz <widy.agung@gmail.com>
2019-01-04 17:41:12 +0700

    testFairy removed due the Google Play Policy

--- SimasPay/app/src/main/java/com/payment/simaspay/services/Validation.java
+++
@@ -1,5 +0,0 @@
-package com.payment.simaspay.services;
-
-public class Validation {
-
-}
--- SimasPay/app/src/main/java/pubdialog/ItemLocation.java
+++
@@ -1,9 +0,0 @@
-package pubdialog;
-
-/**
- * Position type for Item background
- * Created by KokerWang on 15/2/26.
- */
-public enum ItemLocation {
-    TOP, CENTER, BOTTOM, ALONE
-}
